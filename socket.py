6a3bd878f950d720ad2be833c3c373706efd1b0b
ayoub68000 <11275224-a6888b@users.noreply.gitlab.com>
2022-06-13 21:11:58 +0200

    pep8

--- socket.py
+++ socket.py
@@ -1,14 +1,16 @@
 import socket
 
-def start_socket(host: str, port: int=2222):  
+
+def start_socket(host: str, port: int = 2222):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try: 
+    try:
         client.connect((host, port))
     except socket.gaierror:
-        return False 
-    
-    return client 
-     
-def send_file(file: str, conn: socket.socket): 
-    with open(file, 'rb') as f: 
+        return False
+
+    return client
+
+
+def send_file(file: str, conn: socket.socket):
+    with open(file, 'rb') as f:
         conn.sendfile(f)
