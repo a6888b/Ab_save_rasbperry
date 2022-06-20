e1a384dd1f1800a010d6a7e81ea608a489e3afdc
a6888b <ayoubbrahim68@gmail.com>
2022-06-20 22:34:53 +0200

    ajout de la boucle pour ecrire sur le fichier, ajout du split sur les ligne

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import constant
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try: 
-    s.bind((constant.HOST, constant.PORT))
+    s.bind(('', constant.PORT)) # ouvre le socket sur cette la machine 
 except OSError: 
     print('Attendez que le serveur s\' eteigne completement')
     exit()
@@ -15,11 +15,11 @@ while True:
     conn, addr = s.accept()
     contents = conn.recv(1024).decode()
     
-    name_file = contents.split('||||||')[0]
-    content = contents.split('||||||')[1]
+    name_file = contents.split('||||||')[0] # recupere le nom du fichier 
+    lines = contents.split('||||||')[1].split('\n') # recupere le contenue du fichier 
     
-    with open(name_file) as f:
-        print(type(content))
-        # f.write(contents)
+    with open(name_file, 'w') as f:
+        for line in lines: 
+            f.write(line)
      
     
