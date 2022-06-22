6c8a4466d32b7ae1c6e130cea4faaed193274a29
a6888b <ayoubbrahim68@gmail.com>
2022-06-22 22:23:49 +0200

    rajout variable folder_change est reformattage et ajout con dition pour le changement de repertoire

--- server.py
+++ server.py
@@ -3,35 +3,36 @@ import socket
 
 import constant
 
+folder_change = False
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try: 
-    s.bind(('', constant.PORT)) # ouvre le socket sur cette la machine 
-except OSError: 
+
+try:
+    s.bind(('', constant.PORT))  # ouvre le socket sur cette la machine
+except OSError:
     print('Attendez que le serveur s\' eteigne completement')
     exit()
 
 s.listen()
 
-while True: 
+while True:
     conn, addr = s.accept()
     contents = conn.recv(1024).decode()
     
-    if constant.NAME_FOLDER_VARIABLE in contents: # si c'est un nom de dossier qui est envoyez 
+    if folder_change: # condition pour voir si l'arborescence a ete changer 
+        os.chdir('../') # descend d'une arboresence 
+
+    if constant.NAME_FOLDER_VARIABLE in contents:  # si c'est un nom de dossier qui est envoyez
+        folder_change = True 
         folder = contents.split(constant.NAME_FOLDER_VARIABLE)[0]
-        
-        os.mkdir(folder) # creation dossier 
-        os.chdir(folder) # changement repart actuelle par le nouveau creer 
-        
-          
-        
-    else:     
-        name_file = contents.split('||||||')[0] # recupere le nom du fichier 
-        lines = contents.split('||||||')[1].split('\n') # recupere le contenue du fichier 
-    
-    
-    
+
+        os.mkdir(folder)  # creation dossier
+        os.chdir(folder)  # changement repart actuelle par le nouveau creer
+
+    else:
+        name_file = contents.split('||||||')[0]  # recupere le nom du fichier
+        lines = contents.split('||||||')[1].split(
+            '\n')  # recupere le contenue du fichier
+
         with open(name_file, 'w') as f:
-            for line in lines: 
+            for line in lines:
                 f.write(line)
-     
-    
