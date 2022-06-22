cc7dbf886049fc578e6299bfccf680df0fc3a325
a6888b <ayoubbrahim68@gmail.com>
2022-06-22 22:29:02 +0200

    reformattage

--- script.py
+++ script.py
@@ -5,20 +5,23 @@ import constant
 
 
 client = cn.start_socket(constant.HOST, constant.PORT)
+
 while not client is False:
     print('Connexion effectuer')
     path_or_file = input(
         "Entrez le chemin d'accées relatife a votre fichier/dossier: "
     )
-    if path_or_file in constant.ARRAY_WAY_QUIT: # si le client entrez une commande pour stopper la connexion
+    if path_or_file in constant.ARRAY_WAY_QUIT:  # si le client entrez une commande pour stopper la connexion
         client.close()
         exit()
-        
+
     if pt.path_exists(path_or_file):
         if pt.is_file(path_or_file):
             cn.send_file(path_or_file, client)
 
         elif pt.is_folder(path_or_file):
+            cn.send_name_folder(path_or_file, client)
+
             for file in pt.get_content_folder(path_or_file):
                 if pt.is_file(file):
                     cn.send_file(str(file), client)
